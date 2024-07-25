525de90ed6923e1a18e956c565770b9fccfecbef
Nanamimikiki <nam.prog.acc@gmail.com>
2024-07-25 22:53:09 +0300

    no

--- src/main/java/org/mude/config/MinioConfig.java
+++ src/main/java/org/mude/config/MinioConfig.java
@@ -2,7 +2,6 @@ package org.mude.config;
 
 import io.minio.MinioClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.Scope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
