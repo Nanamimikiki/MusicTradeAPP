6a1e07cc10b1141eb7f29c6e9403fc150e44ccc4
Nanamimikiki <nam.prog.acc@gmail.com>
2024-07-25 15:35:40 +0300

    save

--- src/main/java/org/mude/config/MinioConfig.java
+++ src/main/java/org/mude/config/MinioConfig.java
@@ -2,6 +2,7 @@ package org.mude.config;
 
 import io.minio.MinioClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.Scope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/org/mude/rest/AdminRestController.java
+++ src/main/java/org/mude/rest/AdminRestController.java
@@ -1,18 +1,17 @@
 package org.mude.rest;
 
-import lombok.extern.slf4j.Slf4j;
 import org.mude.service.i.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
-@RequestMapping("/api/admin")
 public class AdminRestController {
     @Autowired
     public AdminRestController(UserService userService) {
     }
+
+    @GetMapping(value = "api/admin")
     public String admin(){
         return "ADMIN";
     }
